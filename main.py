cb5aed6f9432064627f8ac2603e98164ddf94a3d
ThatGayKid <64110708+ThatGayKid@users.noreply.github.com>
2020-11-20 23:33:11 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import discord
+from discord.ext import commands
+from discord.ext.commands import Bot, has_permissions, CheckFailure
 import FactGen
 import Loggy
 
@@ -7,17 +9,22 @@ import random
 import fnmatch
 
 TOKEN = ('NzA0MzIwNjkyMTY1OTM1MjE0.Xqbbwg.Dx-R3YZo8N4_gKz43gr0lpMNZ34')
-prefix = ('!factual')
 
 HHelp = ("\
->Help - Displays this menu or gives more information about another command (e.g. !Factual Help Fact)\n\
->Fact - Generates a random Fact (e.g. !Factual Fact)\n\
->Prefix - Changes the bot's prefix(e.g. !Factual Prefix !F)\n\
+> Help - Displays this menu or gives more information about another command\n\
+> Fact - Generates a random Fact\n\
+> Prefix - Changes the bot's prefix\n\
 ")
+
+Help = [\
+'> Help - Gives additional information about a given command',\
+'> Fact - Generates a random ',\
+"> Prefix(Admin Req) - Changes the bot's prefix"\
+]
 
 Commands = ("help","fact","prefix")
 
-client = discord.Client()
+bot = commands.Bot(command_prefix='!FB ')
 
 # ------------------------------------ #
 #               Commands               #
@@ -27,61 +34,57 @@ client = discord.Client()
 #             Chose Command            #
 # ------------------------------------ #
 
-def FindCommand(message):
-    for command in Commands:
-        if fnmatch.fnmatch(message.content.lower(),("*"+command)) == True:
-            Loggy.Add("FindCommand: Running command - "+command,message.author)
-            return command
-        else:
-            Loggy.Add("FindCommand: No Command Selected",message.author)
-            return
+def OrderCommand(message):
+    MessageFiltered = (((message.content).lower()).split())
+    MessageLength = len(MessageFiltered)
+
+#Detect Pefix
+    if (MessageFiltered[0] == Prefix):
+        Loggy.Add("OrderCommand: Running command - "+Commands,message.author)
+        return MessageFiltered[1]
+    
+    elif MessageLength > 1:
+        Loggy.Add("OrderCommand: No Command Selected",message.author)
+        return
 
 # ------------------------------------ #
 #             Main Section             #
 # ------------------------------------ #
-@client.event
+@bot.event
 async def on_ready():
     Loggy.Add("Discord: Bot Started","Discord")
-    
-@client.event
-async def on_message(message):
-    Loggy.Add("Discord: Message detected",message.author)
-    
-#Own Message?
-    if message.author == client.user:
-        return 
-
-#Detect Prefix
-    if fnmatch.fnmatch(str(message.content).lower(),(prefix+"*")) == True:
-        Loggy.Add ("Detect Prefix: Prefix Detected",message.author)
-        Command = FindCommand(message)
-    else:
-        Loggy.Add ("Detect Prefix: Non-Bot message",message.author)
-        return
-
 
 #Fact Command
-    if Command == "fact":
-        Loggy.Add ("Command - Fact: Starting",message.author)
-        await message.channel.send(FactGen.GenerateFact())
+@bot.command(name="Fact")
+async def Generate_Fact(ctx):
+        Loggy.Add ("Command - Fact: Starting",ctx.author)
+        await ctx.channel.send(FactGen.GenerateFact())
         return
 
 #Help Command
-    if Command == "help":
-        Loggy.Add ("Command - Help: Starting",message.author)
-        await message.channel.send(HHelp)
+@bot.command(name="Help")
+async def Help_Menu(ctx):
+        Loggy.Add ("Command - Help: Starting",ctx.author)
+        msg = (HHelp)
+        await ctx.channel.send(msg)
         return
 
 
 #Prefix Command
-    if Command == "prefix":
-        Loggy.Add ("Command - Prefix: ",message.author)
-        await message.channel.send("> W.I.P. Not Avaliable Right Now")
-        return
+@bot.command(pass_context=True)
+@has_permissions(administrator=True)
+async def Prefix(ctx,prefix):
+    Loggy.Add ("Command - Prefix Change to "+prefix+": ",ctx.author)
+    bot.command_prefix = prefix
+    msg = "Prefix changed to ``{prefix}``"
+    await ctx.send(msg)
+    return
 
-#Empty Command
-    else:
-        Loggy.Add (("Empty("+str(Command)+"): Starting"),message.author)
-        await message.channel.send('> Please enter a valid command\n> Try "'+prefix+' help"')
+@Prefix.error
+async def Prefix_error(error, ctx):
+    if isinstance(error, CheckFailure):
+        Loggy.Add ("Command - Prefix Failure: ",ctx.author)
+        msg = "> This is an Admin only command {}".format(ctx.message.author.mention)  
+        await bot.send_message(ctx.message.channel, msg)
         
-client.run(TOKEN)
+bot.run(TOKEN)
