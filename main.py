0ee442fbf2e04abdb6d893cc01296178b4174ff5
ThatGayKid <64110708+ThatGayKid@users.noreply.github.com>
2020-11-21 18:49:16 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# -------------- Imports ------------- #
 import discord
 from discord.ext import commands
 from discord.ext.commands import Bot, has_permissions, CheckFailure
@@ -8,83 +9,57 @@ import time
 import random
 import fnmatch
 
+# ----------- Bot Variables ---------- #
+
 TOKEN = ('NzA0MzIwNjkyMTY1OTM1MjE0.Xqbbwg.Dx-R3YZo8N4_gKz43gr0lpMNZ34')
+bot = commands.Bot(command_prefix="!F ")
+bot.remove_command('help')
 
-HHelp = ("\
-> Help - Displays this menu or gives more information about another command\n\
-> Fact - Generates a random Fact\n\
-> Prefix - Changes the bot's prefix\n\
-")
+# ----------- Main Section ----------- #
 
-Help = [\
-'> Help - Gives additional information about a given command',\
-'> Fact - Generates a random ',\
-"> Prefix(Admin Req) - Changes the bot's prefix"\
-]
-
-Commands = ("help","fact","prefix")
-
-bot = commands.Bot(command_prefix='!FB ')
-
-# ------------------------------------ #
-#               Commands               #
-# ------------------------------------ #
-
-# ------------------------------------ #
-#             Chose Command            #
-# ------------------------------------ #
-
-def OrderCommand(message):
-    MessageFiltered = (((message.content).lower()).split())
-    MessageLength = len(MessageFiltered)
-
-#Detect Pefix
-    if (MessageFiltered[0] == Prefix):
-        Loggy.Add("OrderCommand: Running command - "+Commands,message.author)
-        return MessageFiltered[1]
-    
-    elif MessageLength > 1:
-        Loggy.Add("OrderCommand: No Command Selected",message.author)
-        return
-
-# ------------------------------------ #
-#             Main Section             #
-# ------------------------------------ #
 @bot.event
 async def on_ready():
     Loggy.Add("Discord: Bot Started","Discord")
 
-#Fact Command
-@bot.command(name="Fact")
-async def Generate_Fact(ctx):
-        Loggy.Add ("Command - Fact: Starting",ctx.author)
-        await ctx.channel.send(FactGen.GenerateFact())
-        return
 
-#Help Command
-@bot.command(name="Help")
-async def Help_Menu(ctx):
+# ----------- Help Command ----------- #
+HHelp = ("```\
+Help   - Displays this menu\n\
+Fact   - Generates a random fact\
+```")
+""" Prefix - W.I.P: Changes the bot's prefix (Admin Only)\ """
+    
+@bot.command(name = "Help")
+async def Help(ctx):
         Loggy.Add ("Command - Help: Starting",ctx.author)
         msg = (HHelp)
         await ctx.channel.send(msg)
         return
 
 
-#Prefix Command
-@bot.command(pass_context=True)
+# ----------- Fact Command ----------- #
+@bot.command(name = "Fact")
+async def Fact(ctx):
+        Loggy.Add ("Command - Fact: Starting",ctx.author)
+        await ctx.channel.send(FactGen.GenerateFact())
+        return
+
+""" # ---------- Prefix Command ---------- #
+@bot.command(pass_context=True,name = "Prefix")
 @has_permissions(administrator=True)
 async def Prefix(ctx,prefix):
     Loggy.Add ("Command - Prefix Change to "+prefix+": ",ctx.author)
     bot.command_prefix = prefix
-    msg = "Prefix changed to ``{prefix}``"
+    msg = "`Prefix changed to "+prefix+"`"
     await ctx.send(msg)
     return
 
+# ----------- Prefix Error ----------- #
 @Prefix.error
 async def Prefix_error(error, ctx):
     if isinstance(error, CheckFailure):
         Loggy.Add ("Command - Prefix Failure: ",ctx.author)
-        msg = "> This is an Admin only command {}".format(ctx.message.author.mention)  
+        msg = "`This is an Admin only command`"
         await bot.send_message(ctx.message.channel, msg)
-        
+         """
 bot.run(TOKEN)
